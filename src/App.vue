aca1ec8e46261b2150fd41ba5bbd7cae135c4dca
MyMonsters <1104123836@qq.com>
2024-10-16 11:56:56 +0800

    [FIX]：备案整改

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div class="min-h-700"><router-view></router-view></div>
+  <div style="height: 80px; font-size: large">蜀ICP备2023019073号-1</div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
